b53a9e52f70c2171eb387c96800aff761817cb76
瑞g <2275569272@qq.com>
2025-03-31 16:49:40 +0800

    Add files via upload

---
+++ GitTest.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GitTest : MonoBehaviour
+{
+    public string str = "GitHub≤‚ ‘";
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
