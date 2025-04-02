82939b0e6ba782168642feb26a2b233dd7b7902a
Dashboard <2275569272@qq.com>
2025-04-02 20:15:37 +0800

    第一次提交

--- GitTest.cs
+++ GitTest.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class GitTest : MonoBehaviour
 {
     public string str = "GitHub测试";
+    public string str2 = "第一次提交";
     // Start is called before the first frame update
     void Start()
     {
